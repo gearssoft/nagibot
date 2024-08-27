0307ad0dea0815c88463c0868d29b3bae53fcf3e
gbox3d <gbox3d@gmail.com>
2024-08-27 22:19:34 +0900

    main Cam

--- UITool/UI/compile.sh
+++ UITool/UI/compile.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-pyside6-uic StartupWindow.ui -o StartupWindow.py
+# pyside6-uic StartupWindow.ui -o StartupWindow.py
 pyside6-uic mainForm.ui -o mainForm.py
 pyside6-uic setupForm.ui -o setupForm.py
 pyside6-uic StartUpForm.ui -o StartUpForm.py
+pyside6-uic AppForm.ui -o AppForm.py
 
 echo "Done"
  
--- UITool/mainForm.py
+++ UITool/mainForm.py
@@ -1,44 +1,80 @@
 import sys
-from PySide6.QtWidgets import QApplication ,QWidget
-from PySide6.QtCore import Signal
+from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtCore import Signal, QTimer, Qt, QThread
+from PySide6.QtGui import QImage, QPixmap
+import cv2
+import numpy as np
 
-# import icons_rc
 import UI.mainForm
 
-class MainForm(QWidget,UI.mainForm.Ui_mainForm):
+class VideoThread(QThread):
+    change_pixmap_signal = Signal(np.ndarray)
+
+    def __init__(self, rtsp_url):
+        super().__init__()
+        self.rtsp_url = rtsp_url
+        self._run_flag = True
+
+    def run(self):
+        cap = cv2.VideoCapture(self.rtsp_url)
+        while self._run_flag:
+            ret, cv_img = cap.read()
+            if ret:
+                self.change_pixmap_signal.emit(cv_img)
+        cap.release()
+
+    def stop(self):
+        self._run_flag = False
+        self.wait()
+
+class MainForm(QWidget, UI.mainForm.Ui_mainForm):
     
-    gotoHomeSignal = Signal()  # 새로운 시그널 추가
+    gotoHomeSignal = Signal()
     gotoSetupSignal = Signal()
     closedSignal = Signal()
     
-    def __init__(self,parent=None):
+    def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
         
         self.btnGoHome.clicked.connect(self.gotoHome)
         self.btnGotoSetup.clicked.connect(self.gotoSetup)
         
+        # 초기 "준비 중" 메시지 표시
+        self.mainCamScreen_bmpLabel.setText("영상 준비 중...")
+        
+        # RTSP 스트림 설정
+        self.rtsp_url = "rtsp://gbox3d:71021707@gears001.iptime.org:21028/stream_ch00_0"
+        
+        # 비디오 스레드 생성 및 시작
+        self.thread = VideoThread(self.rtsp_url)
+        self.thread.change_pixmap_signal.connect(self.update_image)
+        self.thread.start()
+        
+    def update_image(self, cv_img):
+        """비디오 프레임을 업데이트하는 메서드"""
+        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+        p = convert_to_Qt_format.scaled(self.mainCamScreen.size(), Qt.KeepAspectRatio)
+        self.mainCamScreen_bmpLabel.setPixmap(QPixmap.fromImage(p))
+        
     def gotoHome(self):
         print("gotoHome")
-        # startup form으로 이동
         self.gotoHomeSignal.emit()
         
     def gotoSetup(self):
         print("gotoSetup")
-        # setup form으로 이동
         self.gotoSetupSignal.emit()
         
     def closeEvent(self, event):
         print("closeEvent")
-        # if self.parent() is not None:
-        #     self.parent().close()
+        self.thread.stop()
         self.closedSignal.emit()
-        
         super().closeEvent(event)
 
-        
 if __name__ == '__main__':
-    
     theApp = QApplication(sys.argv)
     form = MainForm()
     form.show()
