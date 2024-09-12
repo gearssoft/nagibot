c145f260bb8b0fc08f2f4f7c7135338b618f8789
gbox3d <gbox3d@gmail.com>
2024-09-12 20:16:59 +0900

    ....

--- UITool/mainForm.py
+++ UITool/mainForm.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtCore import Signal, QTimer, Qt, QThread
+from PySide6.QtCore import Signal, QTimer, Qt, QThread,Slot
 from PySide6.QtGui import QImage, QPixmap
 import cv2
 import numpy as np
@@ -40,6 +40,36 @@ class MainForm(QWidget, UI.mainForm.Ui_mainForm):
         self.btnGoHome.clicked.connect(self.gotoHome)
         self.btnGotoSetup.clicked.connect(self.gotoSetup)
         
+        # 키 버튼 눌림/떨어짐 표시용 레이블 숨김
+        self.label_keyup_normal.setVisible(True)
+        self.label_keyup_push.setVisible(False)
+        
+        self.label_keydown_normal.setVisible(True)
+        self.label_keydown_push.setVisible(False)
+        
+        self.label_keyleft_normal.setVisible(True)
+        self.label_keyleft_push.setVisible(False)
+        
+        self.label_keyright_normal.setVisible(True)
+        self.label_keyright_push.setVisible(False)
+        
+        
+        # Connect the button pressed and released signals to show and hide the labels
+        self.btnKeyUp.pressed.connect(self.keyUpPressed)
+        self.btnKeyUp.released.connect(self.keyUpReleased)
+        
+        self.btnKeyDown.pressed.connect(self.keyDownPressed)
+        self.btnKeyDown.released.connect(self.keyDownReleased)
+        
+        self.btnKeyLeft.pressed.connect(self.keyLeftPressed)
+        self.btnKeyLeft.released.connect(self.keyLeftReleased)
+        
+        self.btnKeyRight.pressed.connect(self.keyRightPressed)
+        self.btnKeyRight.released.connect(self.keyRightReleased)
+        
+        
+        # self.btnKeyUp.clicked.connect(self.keyUp)
+        
         # 초기 "준비 중" 메시지 표시
         self.mainCamScreen_bmpLabel.setText("영상 준비 중...")
         
@@ -60,14 +90,57 @@ class MainForm(QWidget, UI.mainForm.Ui_mainForm):
         p = convert_to_Qt_format.scaled(self.mainCamScreen.size(), Qt.KeepAspectRatio)
         self.mainCamScreen_bmpLabel.setPixmap(QPixmap.fromImage(p))
         
+    @Slot()
     def gotoHome(self):
         print("gotoHome")
         self.gotoHomeSignal.emit()
-        
+    
+    @Slot()
     def gotoSetup(self):
         print("gotoSetup")
         self.gotoSetupSignal.emit()
         
+    @Slot()
+    def keyUpPressed(self):
+        self.label_keyup_normal.setVisible(False)
+        self.label_keyup_push.setVisible(True)
+    
+    @Slot()
+    def keyUpReleased(self):
+        self.label_keyup_normal.setVisible(True)
+        self.label_keyup_push.setVisible(False)
+        
+    @Slot()
+    def keyDownPressed(self):
+        self.label_keydown_normal.setVisible(False)
+        self.label_keydown_push.setVisible(True)
+    
+    @Slot()
+    def keyDownReleased(self):
+        self.label_keydown_normal.setVisible(True)
+        self.label_keydown_push.setVisible(False)
+        
+    @Slot()
+    def keyLeftPressed(self):
+        self.label_keyleft_normal.setVisible(False)
+        self.label_keyleft_push.setVisible(True)
+    
+    @Slot()
+    def keyLeftReleased(self):
+        self.label_keyleft_normal.setVisible(True)
+        self.label_keyleft_push.setVisible(False)
+        
+    @Slot()
+    def keyRightPressed(self):
+        self.label_keyright_normal.setVisible(False)
+        self.label_keyright_push.setVisible(True)
+        
+    @Slot()
+    def keyRightReleased(self):
+        self.label_keyright_normal.setVisible(True)
+        self.label_keyright_push.setVisible(False)
+        
+    
     def closeEvent(self, event):
         print("closeEvent")
         self.thread.stop()
